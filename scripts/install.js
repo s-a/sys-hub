c9f2ade7d2fda28a98d9e8472889b213103e4f35
s-a <stephan.ahlf@googlemail.com>
2016-03-23 22:45:50 +0100

    :bee:

--- scripts/install.js
+++ scripts/install.js
@@ -3,5 +3,5 @@ var isWin = /^win/.test(process.platform);
 if (isWin){
 	var fs = require('fs');
 	var path = require('path');
-	fs.writeFileSync(path.join(__dirname, "..", "..", "..", "show.cmd"), "node " +  path.join(__dirname, "..", "lib/cli.js") );
+	fs.writeFileSync(path.join(__dirname, "..", "..", "..", "sys-hub.cmd"), "node " +  path.join(__dirname, "..", "lib/cli.js") );
 }
