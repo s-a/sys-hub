d0c9852caf5722fb85aad853ff47e991be4d4a5e
s-a <stephan.ahlf@googlemail.com>
2016-03-24 07:41:01 +0100

    fix she-bang

--- scripts/install.js
+++
@@ -1,7 +0,0 @@
-var isWin = /^win/.test(process.platform);
-
-if (isWin){
-	var fs = require('fs');
-	var path = require('path');
-	fs.writeFileSync(path.join(__dirname, "..", "..", "..", "sys-hub.cmd"), "node " +  path.join(__dirname, "..", "lib/cli.js") );
-}
